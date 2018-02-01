d368f114cb51c0229c2c2230788b5986a41764c5
RSPSuresh1 <35728252+RSPSuresh1@users.noreply.github.com>
2018-02-01 16:26:26 +0530

    Update hello.C

--- hello.C
+++ hello.C
@@ -4,7 +4,7 @@ void main()
 {
     int  b,i;
     clrscr();
-    printf("Enter the  no of times:\n");
+    printf("Enter the no of times:\n");
     scanf("%d",&b);
     for(i=1;i<=b;i++)
     printf("\n\nHello");
