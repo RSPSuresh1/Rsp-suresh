c9d31c1c6983d695e85005229d1f264eb3252658
RSPSuresh1 <35728252+RSPSuresh1@users.noreply.github.com>
2018-02-01 16:25:31 +0530

    Create hello.C

---
+++ hello.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int  b,i;
+    clrscr();
+    printf("Enter the  no of times:\n");
+    scanf("%d",&b);
+    for(i=1;i<=b;i++)
+    printf("\n\nHello");
+    getch();
+    }
