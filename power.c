895df4abce0699e00c3c33fb1aff1320edd3ee74
RSPSuresh1 <35728252+RSPSuresh1@users.noreply.github.com>
2018-02-06 15:44:14 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int a,b,c;
+    clrscr();
+    printf("Enter thfirst number ");
+    scanf("%d",&a);
+    printf("Enter the sond number ");
+    scanf("%d",&b);
+    c=a^b;
+    printf("%d",c);
+    getch();
+    return 0;
+ }   
