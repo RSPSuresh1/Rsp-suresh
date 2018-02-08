e6c4acd9eeb31a5a5d13bbb204b2ed44367f915d
RSPSuresh1 <35728252+RSPSuresh1@users.noreply.github.com>
2018-02-08 15:47:33 +0530

    Create factorial.C

---
+++ factorial.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int i,n,f=1,p;
+    
+    scanf("%d",&n);
+    
+    
+    for(i=0;n!=0;i++,n--)    
+    {
+        f=f*n;
+        
+    }
+     printf("%d is",f);
+}
+    
